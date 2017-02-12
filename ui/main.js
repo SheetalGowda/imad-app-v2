cd610708b3a1357f140289edea8ef4ba2a5904f5
SheetalGowda <sheetal.sgowdaa@gmail.com>
2017-02-12 12:20:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,13 @@
 console.log('Loaded!');
 alert("welocome");
+var img=document.getElementById("madi");
+var marginleft=0;
+function moveRight()
+{
+marginleft=marginleft+10;
+img.style.marginleft=marginleft+px;
+}
+img.OnClick()=function()
+{
+    var interval=setInterval(moveRight,50);
+}
